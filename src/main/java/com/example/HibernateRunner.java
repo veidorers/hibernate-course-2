2523130e28b3839f23198e1ec4710f087dfd170d
Sergey Fomenko <artem.work749@gmail.com>
2023-10-30 13:18:59 +0300

    Session and SessionFactory

---
+++ src/main/java/com/example/HibernateRunner.java
@@ -0,0 +1,18 @@
+package com.example;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateRunner {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
+             Session session = sessionFactory.openSession()) {
+            System.out.println("OK");
+        }
+    }
+}
