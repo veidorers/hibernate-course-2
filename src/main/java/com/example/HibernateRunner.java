61ee15ce3e524172da9e21271a8d648569ad2fc5
Sergey Fomenko <artem.work749@gmail.com>
2023-10-30 13:49:04 +0300

    Entity

--- src/main/java/com/example/HibernateRunner.java
+++ src/main/java/com/example/HibernateRunner.java
@@ -1,18 +1,35 @@
 package com.example;
 
 
+import com.example.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.cfg.Configuration;
+
+import java.time.LocalDate;
 
 public class HibernateRunner {
     public static void main(String[] args) {
         Configuration configuration = new Configuration();
+        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
         configuration.configure();
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
-            System.out.println("OK");
+            session.beginTransaction();
+
+            User user = User.builder()
+                    .username("ivan2@gmail.com")
+                    .firstname("Ivan")
+                    .lastname("Ivanov")
+                    .birthDate(LocalDate.now())
+                    .age(1)
+                    .build();
+
+            session.persist(user);
+
+            session.getTransaction().commit();
         }
     }
 }
---
+++ src/main/java/com/example/entity/User.java
@@ -0,0 +1,27 @@
+package com.example.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    private String username;
+    private String firstname;
+    private String lastname;
+    private LocalDate birthDate;
+    private Integer age;
+}
