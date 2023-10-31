3f4c0e067f8c3282ceb59f1cf61c0aae048921c6
Sergey Fomenko <artem.work749@gmail.com>
2023-10-31 14:14:05 +0300

    Logging - log4j.xml file

--- src/main/java/com/example/HibernateRunner.java
+++ src/main/java/com/example/HibernateRunner.java
@@ -5,32 +5,35 @@ import com.example.entity.User;
 import com.example.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HibernateRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(HibernateRunner.class);
+
     public static void main(String[] args) {
         User user = User.builder()
-                .username("ivan@gmail.com")
+                .username("ivan910@gmail.com")
                 .firstname("Ivan")
                 .lastname("Ivanov")
                 .build();
+        log.info("user is in transient state, object: {} ", user);
+
         try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
             try (Session session1 = sessionFactory.openSession()) {
-                session1.beginTransaction();
+                Transaction transaction = session1.beginTransaction();
+                log.trace("Transaction is created, {}", transaction);
 
-//                session1.persist(user);
+                session1.persist(user);
+                log.trace("User is in persistent state, object: {}, session: {}", user, session1);
 
                 session1.getTransaction().commit();
             }
-
-            try (Session session2 = sessionFactory.openSession()) {
-                session2.beginTransaction();
-
-                User user2 = session2.get(User.class, user.getUsername());
-                user2.setFirstname("Abdul");
-                session2.refresh(user2);
-
-                session2.getTransaction().commit();
-            }
+            log.warn("user is in detached state, object: {}, session is closed", user);
+        } catch(Exception e) {
+            log.error("Exception occurred", e);
         }
 
     }
