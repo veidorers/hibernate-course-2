782c15cdcf87c7a45439ad05ac7890f115b74e71
Sergey Fomenko <artem.work749@gmail.com>
2023-10-31 13:12:13 +0300

    Entity Lifecycle

--- src/main/java/com/example/HibernateRunner.java
+++ src/main/java/com/example/HibernateRunner.java
@@ -1,51 +1,37 @@
 package com.example;
 
 
-import com.example.entity.Birthday;
-import com.example.entity.Role;
 import com.example.entity.User;
-import com.example.entity.converter.BirthdayConverter;
-import io.hypersistence.utils.hibernate.type.json.JsonBinaryType;
+import com.example.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
-import org.hibernate.cfg.Configuration;
-
-import java.time.LocalDate;
 
 public class HibernateRunner {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration();
-        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
-        configuration.addAttributeConverter(new BirthdayConverter());
-        configuration.configure();
+        User user = User.builder()
+                .username("ivan@gmail.com")
+                .firstname("Ivan")
+                .lastname("Ivanov")
+                .build();
+        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
+            try (Session session1 = sessionFactory.openSession()) {
+                session1.beginTransaction();
 
-        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
-             Session session = sessionFactory.openSession()) {
-            session.beginTransaction();
+//                session1.persist(user);
 
-            User user = User.builder()
-                    .username("ivan5@gmail.com")
-                    .info("""
-                            {
-                                "name": "Ivan Ivanov",
-                                "age": 21
-                            }
-                            """)
-                    .firstname("Ivan")
-                    .lastname("Ivanov")
-                    .birthDate(new Birthday(LocalDate.of(2000, 10, 25)))
-                    .role(Role.ADMIN)
-                    .build();
+                session1.getTransaction().commit();
+            }
 
-            session.merge(user);
-            User user1 = session.get(User.class, "ivan1@gmail.com");
-            user1.setLastname("Ivanov");
-            System.out.println(session.isDirty());
-            session.flush();
-            System.out.println(session.isDirty());
+            try (Session session2 = sessionFactory.openSession()) {
+                session2.beginTransaction();
 
-            session.getTransaction().commit();
+                User user2 = session2.get(User.class, user.getUsername());
+                user2.setFirstname("Abdul");
+                session2.refresh(user2);
+
+                session2.getTransaction().commit();
+            }
         }
+
     }
 }
---
+++ src/main/java/com/example/util/HibernateUtil.java
@@ -0,0 +1,19 @@
+package com.example.util;
+
+import com.example.entity.converter.BirthdayConverter;
+import lombok.experimental.UtilityClass;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
+import org.hibernate.cfg.Configuration;
+
+@UtilityClass
+public class HibernateUtil {
+    public static SessionFactory buildSessionFactory() {
+        Configuration configuration = new Configuration();
+        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+        configuration.addAttributeConverter(new BirthdayConverter());
+        configuration.configure();
+
+        return configuration.buildSessionFactory();
+    }
+}
