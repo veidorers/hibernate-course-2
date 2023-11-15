4121e6368b1fa4e56673501d4ef1ad415b8c5b6e
Sergey Fomenko <artem.work749@gmail.com>
2023-11-15 15:45:16 +0300

    Hibernate Envers. Part 3

--- src/main/java/com/example/HibernateRunner.java
+++ src/main/java/com/example/HibernateRunner.java
@@ -6,20 +6,36 @@ import com.example.entity.User;
 import com.example.util.HibernateUtil;
 import com.example.util.TestDataImporter;
 import jakarta.transaction.Transactional;
+import org.hibernate.Session;
+import org.hibernate.envers.AuditReader;
+import org.hibernate.envers.AuditReaderFactory;
+import org.hibernate.envers.query.AuditEntity;
 
 public class HibernateRunner {
     @Transactional
     public static void main(String[] args) {
-        try (var sessionFactory = HibernateUtil.buildSessionFactory();
-             var session = sessionFactory.openSession()) {
-            TestDataImporter.importData(sessionFactory);
+        try (var sessionFactory = HibernateUtil.buildSessionFactory()) {
+            try (var session = sessionFactory.openSession()) {
+                session.beginTransaction();
 
-            session.beginTransaction();
+                var payment = session.get(Payment.class, 1L);
+                payment.setAmount(payment.getAmount() + 10);
 
-            var payment = session.get(Payment.class, 1L);
-            payment.setAmount(payment.getAmount() + 10);
+                session.getTransaction().commit();
+            }
+            try(var session2 = sessionFactory.openSession()) {
+                session2.beginTransaction();
 
-            session.getTransaction().commit();
+                var auditReader = AuditReaderFactory.get(session2);
+                var oldPayment = auditReader.find(Payment.class, 1L, 1699974409653L);
+
+                auditReader.createQuery()
+                        .forEntitiesAtRevision(Payment.class, 400L)
+                        .add(AuditEntity.and(AuditEntity.property("amount").ge(450), AuditEntity.property("amount").lt(600)))
+                        .getResultList();
+
+                session2.getTransaction().commit();
+            }
         }
     }
 }
